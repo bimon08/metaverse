bc58906827805f5f807f65e4d6f54a02da22d8ae
Bimon <130655556+bimon08@users.noreply.github.com>
2023-09-12 03:32:34 +0000

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import {navVariants} from '../utils/motion';
+
+
 const Navbar = () => (
   <nav>
     navbar
